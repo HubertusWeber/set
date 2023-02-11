136895dbd76d2a744d8e2ae75c07adcccd6ae479
Hubertus Weber <1051260+HubertusWeber@users.noreply.github.com>
2023-02-11 15:14:49 +0100

    switched to library as only target

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,14 @@
-#![allow(dead_code)]
-
-use std::{fs::File, io::BufRead, io::BufReader};
-
-use anyhow::Result;
-
 mod display;
 mod lexer;
 mod parser;
 mod transformer;
 
-pub fn run() -> Result<()> {
-    let input_file = File::open("input.txt")?;
-    let input_reader = BufReader::new(input_file);
-    for line in input_reader.lines() {
-        let tokens = lexer::tokanize(line?)?;
-        let syntax_tree = parser::parse(tokens)?;
-        println!("{}", syntax_tree);
-        let result = syntax_tree.transform();
-        println!("{}", result);
+pub fn run(input: &str) -> String {
+    match lexer::tokanize(input.into()) {
+        Err(e) => e.to_string(),
+        Ok(tokens) => match parser::parse(tokens) {
+            Err(e) => e.to_string(),
+            Ok(syntax_tree) => syntax_tree.transform().to_string(),
+        },
     }
-    Ok(())
 }
